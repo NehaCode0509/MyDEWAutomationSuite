820d01ea5d3a40ea92e45c11fe52c33a689bac8a
SSingh2 <SSingh2@DEW214542.env.sa.gov.au>
2025-01-07 21:13:36 +1030

    commit new pom

--- src/test/java/com/DEW/DEWAutomationSuite/atdd/testrunner/TestRunner.java
+++ src/test/java/com/DEW/DEWAutomationSuite/atdd/testrunner/TestRunner.java
@@ -11,7 +11,7 @@ package com.DEW.DEWAutomationSuite.atdd.testrunner;
 	@CucumberOptions(features = "src/test/resources/functional", 
 	glue = { "com.DEW.DEWAutomation.atdd.stepdefinitions", },
 	dryRun = false,
-	plugin = { "pretty","html:target/cucumber-reports.html", "json:target/cucumber-report.json" }, 
+	plugin = { "pretty","html:target/cucumber-reports.html", "json:target/cucumber-report/cucumber.json" }, 
 	monochrome = true,
 	tags = "@WellPermit")
 	 
